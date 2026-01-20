aabdc1365e457a758ca9da2efc03cd99503bec15
W41D0 <walidogamer48@gmail.com>
2026-01-20 22:04:32 +0300

    sheep spawning

--- Assets/Scripts/Sheep Spawn.cs
+++ Assets/Scripts/Sheep Spawn.cs
@@ -1,16 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class SheepSpawn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject Sheep;
+    Transform pos1;
+    Transform pos2;
+    
     void Start()
+    {
+        pos1 = transform.GetChild(0);
+        pos2 = transform.GetChild(1);
+
+        StartCoroutine(SpawnScript(3f));
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnScript(float loopTime)
     {
-        
+        while(true)
+        {
+            float posX = Random.Range(pos1.position.x, pos2.position.x);
+            float posY = Random.Range(pos1.position.y, pos2.position.y);
+            Vector2 spawnPosition = new Vector2(posX, posY);
+
+            Instantiate(Sheep, spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(loopTime);
+        }
     }
 }
